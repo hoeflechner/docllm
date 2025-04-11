3d4fe13ee50a2481982b8b4beee68e0906c11829
Bernhard Hoeflechner <bernhard.hoeflechner@siemens.com>
2025-04-11 15:39:24 +0200

    download files, docker

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.12-slim
+
+# Keeps Python from generating .pyc files in the container
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Turns off buffering for easier container logging
+ENV PYTHONUNBUFFERED=1
+
+RUN python -m pip install --upgrade pip
+
+COPY requirements.txt requirements.txt
+RUN python -m pip install -r requirements.txt
+COPY docllm.py docllm.py 
+
+CMD ["streamlit", "run", "docllm.py"]
--- docllm.py
+++ docllm.py
@@ -1,6 +1,9 @@
+import base64
+import sys
 import hashlib
 import glob
 import mimetypes
+import logging
 from pathlib import Path
 import streamlit as st
 from PyPDF2 import PdfReader
@@ -21,7 +24,18 @@ OLLAMA_URL = os.getenv('OLLAMA_URL', 'http://localhost:11434')
 OLLAMA_MODEL = os.getenv('OLLAMA_MODEL', 'qwen2.5-coder')
 OLLAMA_EMBEDDING_MODEL = os.getenv(
     'OLLAMA_EMBEDDING_MODEL', 'snowflake-arctic-embed2')
-IMPORT_PATH = os.getenv('IMPORT_PATH', 'testfiles/**')
+IMPORT_PATH = os.getenv('IMPORT_PATH', 'testfiles/')
+ORIG_URL = os.getenv('ORIG_URL', 'testfiles/')
+
+PROMPT=os.getenv('PROMPT',
+                 """
+                You are a helpful assistant who summarizes information and answers questions.
+                You respond in the same language the question was asked 
+                not in the language the context was given!
+                If you don't know something, respond with 'I'm sorry, I don't know!'.
+                Be structured, short but accurate.
+                You get the information you need from the following context.
+                """)
 
 def embed(text):
     if st.session_state.get("embeddings",False):
@@ -61,6 +75,33 @@ def import_glob(glob_str):
     files = glob.glob(glob_str,recursive = True)
     import_files(files)
 
+def describe(file_name):
+    llm = ChatOllama(
+        model=OLLAMA_MODEL,
+        temperature=0,
+        verbose=True,
+        base_url=OLLAMA_URL,
+    )
+
+    message = HumanMessage(
+        content=[
+            {"type": "text", "text": 
+             """
+              describe the image in great detail. transcribe any text in the image.
+             """},
+            {
+                "type": "image_url",
+                "image_url": {"url": f"{file_name}"},
+            },
+        ],
+    )
+    try:
+        response = llm.invoke([message])
+        return response.content
+    except Exception as e:
+        print(e)
+        return ""
+
 def import_files(files):
     for item in files:
         if isinstance(item, Path) or isinstance(item, str):
@@ -90,11 +131,14 @@ def import_files(files):
             pdf_reader = PdfReader(file)
             for page in pdf_reader.pages:
                 text += page.extract_text()
+        if isinstance(filetype,str) and filetype.startswith("image/"):
+            text = ""
+            text = describe(file.name)
 
         file.close()
 
         if text == "":
-            print(f"skipping - could not read")
+            print(f"skipping - could not read {filetype}")
             continue
 
         text_splitter = RecursiveCharacterTextSplitter(
@@ -127,23 +171,26 @@ def get_response(user_query, chat_history):
         doc={"id":id, "page_content":data["documents"][0][i], "metadatas":data["metadatas"][0][i]}
         st.session_state.documents[id] = doc
     
+    with st.sidebar:
+        st.write("Sources")
+        files=[doc['metadatas']['filename'] for doc in st.session_state.documents.values()]
+        for file in set(files):
+            file_name=file.replace(IMPORT_PATH,"")
+            file_url=f"{ORIG_URL}/{file_name}"
+            print(IMPORT_PATH)
+            print(f"Source: {file_name} -> {file_url}")
+            st.link_button(file_name, file_url)
+                    
     context = ""
 
     for doc in st.session_state.documents.values():
-        print(f"context from {doc['metadatas']['filename']}")
         st.session_state.documents[doc["id"]] = doc
         context += doc["page_content"]+" \n"
     
     prompt = ChatPromptTemplate.from_messages(
         [
             (
-                "system", 
-                """
-                Du bist ein hilfreicher Assistent, der Informationen zusammenfasst und Fragen beantwortet.
-                Wenn du etwas nicht weisst dann antworte mit "Tut mir leid. das weiss ich nicht!"
-                Strukturiere deine Antwort mit Listen und Tabellen! Sei knapp aber genau.
-                Der Kontext: {context}
-                """               
+                "system", PROMPT+" Context: {context}"              
             ),
             #("placeholder","{context}"),
             ("placeholder", "{chat_history}"),
@@ -178,13 +225,6 @@ def main():
             AIMessage(content="Hi!"),
         ]
 
-    if "imported" not in st.session_state:
-        print(f"starting import from {IMPORT_PATH}")
-        import_glob(IMPORT_PATH+"/**")
-        print("import done")
-
-        st.session_state.imported=True
-
     # conversation
     for message in st.session_state.chat_history:
         if isinstance(message, AIMessage):
@@ -207,17 +247,12 @@ def main():
                 user_query, st.session_state.chat_history))
 
         st.session_state.chat_history.append(AIMessage(content=response))
-
-    # with st.sidebar:
-    #     st.title("Menu:")
-    #     pdf_doc = st.file_uploader(
-    #         "Upload your Files and Click on the Submit & Process Button", accept_multiple_files=True)
-    #     if st.button("Submit & Process"):
-    #         with st.spinner("Processing..."):
-    #             import_files(pdf_doc)
-    #             st.success("Done")
-
-
+    
 if __name__ == "__main__":
-    import_glob(IMPORT_PATH)
-    main()
+    for name, l in logging.root.manager.loggerDict.items():
+        if "streamlit" in name:
+            l.disabled = True
+    if st.runtime.exists():
+        main()
+    else:
+       import_glob(f"{IMPORT_PATH}/**")
